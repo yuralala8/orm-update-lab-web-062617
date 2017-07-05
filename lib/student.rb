67575903f037e50fc909be5919e70c37487737b7
yuralala8 <yc1435@nyu.edu>
2017-07-05 15:51:53 -0400

    orm update lab

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,68 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade, :id
+
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+end
+
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(array)
+    student = self.new(array[0], array[1], array[2])
+  end
+
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    array = DB[:conn].execute(sql, name)[0]
+    self.new(array[0], array[1], array[2])
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
